438d562530d3e8f1030e07c123368513af01a46b
ツキミヤ <49361292+deceitya@users.noreply.github.com>
2022-12-06 02:29:20 +0900

    コードの整形

--- src/main/kotlin/mc/tsukimiya/mooney/core/exception/NegativeMoneyException.kt
+++ src/main/kotlin/mc/tsukimiya/mooney/core/exception/NegativeMoneyException.kt
@@ -1,6 +1,6 @@
 package mc.tsukimiya.mooney.core.exception
 
-import java.util.UUID
+import java.util.*
 
 class NegativeMoneyException(val id: UUID, msg: String? = null) : RuntimeException(msg) {
 }
