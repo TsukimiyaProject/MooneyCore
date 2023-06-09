a3de6d838201094688f64064367e0469ac96c80f
deceitya <49361292+deceitya@users.noreply.github.com>
2023-06-09 19:24:32 +0900

    新規登録に対応

--- src/main/kotlin/mc/tsukimiya/mooney/core/usecase/StoreAccountUseCase.kt
+++ src/main/kotlin/mc/tsukimiya/mooney/core/usecase/StoreAccountUseCase.kt
@@ -7,10 +7,37 @@ import mc.tsukimiya.mooney.core.domain.Money
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
+/**
+ * アカウント保存
+ *
+ * @property repository
+ */
 class StoreAccountUseCase(private val repository: AccountRepository) {
-    fun execute(id: UUID, name: String, amount: Int) {
+    /**
+     * nameとmoneyはnullでなければ値の変更があると見なし、値の保存を行う
+     * 変更があるときのみ、execute(id, name = 〇〇, money = 〇〇)で呼び出す
+     *
+     * @param id
+     * @param name  変更があるときに代入
+     * @param money 変更があるときに代入
+     */
+    fun execute(id: UUID, name: String? = null, money: Int? = null) {
         transaction {
-            repository.store(Account(id, MinecraftId(name), Money(amount)))
+            // アカウント取得
+            var account = repository.find(id)
+
+            if (account != null) {
+                // 引数がnullでなければ値の変更があるとみなし、代入を行う
+                if (name != null) account.name = MinecraftId(name)
+                if (money != null) account.money = Money(money)
+            } else {
+                require(name != null)
+                require(money != null)
+
+                account = Account(id, MinecraftId(name), Money(money))
+            }
+
+            repository.store(account)
         }
     }
 }
