e69951bc301a9cb80703170e6b4773d954e66e00
deceitya <49361292+deceitya@users.noreply.github.com>
2023-09-11 20:45:41 +0900

    インターフェースにコメント追加

--- src/main/kotlin/mc/tsukimiya/mooney/core/MooneyCoreAPI.kt
+++ src/main/kotlin/mc/tsukimiya/mooney/core/MooneyCoreAPI.kt
@@ -1,6 +1,6 @@
 package mc.tsukimiya.mooney.core
 
-import java.util.UUID
+import java.util.*
 
 /**
  * APIのインターフェース
@@ -13,35 +13,36 @@ interface MooneyCoreAPI {
      * 現在のプレイヤーの所持金を取得する
      *
      * @param uuid プレイヤーのUUID
-     * @return 所持金 アカウントが存在しない場合はnull
+     * @return 所持金
      */
-    fun getMoney(uuid: UUID): ULong?
+    fun getMoney(uuid: UUID): MooneyCoreAPIResult
 
     /**
      * プレイヤーの所持金を設定する
      *
      * @param uuid プレイヤーのUUID
      * @param amount 設定額
+     * @return 設定した後の所持金
      */
-    fun setMoney(uuid: UUID, amount: ULong): Boolean
+    fun setMoney(uuid: UUID, amount: ULong): MooneyCoreAPIResult
 
     /**
      * プレイヤーの所持金を増やす
      *
      * @param uuid プレイヤーのUUID
      * @param amount 増やす額
-     * @return 成功したか
+     * @return 増やした後の所持金
      */
-    fun increaseMoney(uuid: UUID, amount: ULong): Boolean
+    fun increaseMoney(uuid: UUID, amount: ULong): MooneyCoreAPIResult
 
     /**
      * プレイヤーの所持金を減らす
      *
      * @param uuid プレイヤーのUUID
      * @param amount 減らす額
-     * @return 成功したか
+     * @return 減らした後の所持金
      */
-    fun decreaseMoney(uuid: UUID, amount: ULong): Boolean
+    fun decreaseMoney(uuid: UUID, amount: ULong): MooneyCoreAPIResult
 
     /**
      * プレイヤーからプレイヤーへお金を支払う
@@ -49,9 +50,9 @@ interface MooneyCoreAPI {
      * @param from 支払元
      * @param to 支払先
      * @param amount 支払額
-     * @return 成功したか
+     * @return 支払った後の所持金
      */
-    fun payMoney(from: UUID, to: UUID, amount: ULong): Boolean
+    fun payMoney(from: UUID, to: UUID, amount: ULong): Pair<MooneyCoreAPIResult, MooneyCoreAPIResult>
 
     /**
      * アカウント作成
